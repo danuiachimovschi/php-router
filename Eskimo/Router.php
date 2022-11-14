7391a412e7048cc9641c63d46fe86fea8c43a7da
danuiachimosvchi <iachimovschid@mail.ru>
2022-11-14 19:08:55 +0200

    match pattern and url

--- Eskimo/Router.php
+++ Eskimo/Router.php
@@ -6,6 +6,10 @@ namespace Eskimo;
 
 use ErrorException;
 
+/**
+ * Router Class
+ * @author Iachimovschi Daniel
+ */
 class Router
 {
     /**
@@ -26,7 +30,14 @@ class Router
         public string $basePath = '/'    
     ){}
 
+    public const REGEX = [
+        'a-z*' => 'a-zA-Z',
+        '0-9' => '0-9',
+        '*' => '\s\S'
+    ];
+
     /**
+     * Crate Route Path 
      * @param string $method HTTP Methods GET,POST,PATCH,PUT,DELETE,HEAD,OPTIONS
      * @param string $path path of URL for 
      * @param callable|array $callback can be a callback function or array with object data [$obj::class, method] 
@@ -48,9 +59,20 @@ class Router
      * Get Matches from URL
      * @return void
      */
-    public static function run(): void
+    public function run(): void
     {
-        
+        // dump($this->routes);
+        $path = $this->getCurrentUrl();
+        $httpMethod = $_SERVER['REQUEST_METHOD'];
+        if ($this->routes) {
+            foreach($this->routes as $router){
+               $regexPattern = $this->pathToRegex($router[1]);
+               if (preg_match($regexPattern, $path)) {
+                    $router[2]();
+                    return;
+               }
+            }
+        }
     }
 
     /**
@@ -59,5 +81,24 @@ class Router
     public function create(string $name, ?array $params): string
     {
         return "test";
+    }
+
+    /**
+     * @return string Current HTTP Url
+     */
+    public function getCurrentUrl(): string
+    {
+        return rtrim(parse_url($_SERVER['REQUEST_URI'])['path'], '/');
+    }
+
+    /**
+     * @return string Return Regex Expresion For Matching
+     */
+    public function pathToRegex($router): string
+    {
+        if (preg_match("/\/{(?P<filter>[\s\S]+)}/i", $router)) {
+            $router = preg_replace("/\/{(.*?)}/i", "/(.*?)", $router);
+        }
+        return "/^" .str_replace('/', '\/', $router) ."$/";
     }
 }
--- index.php
+++ index.php
@@ -2,24 +2,30 @@
 
 use Eskimo\Router;
 
+// use Eskimo\Router;
+
 require __DIR__ . '/vendor/autoload.php';
 
 $router = new Router();
 
 $router->add('GET', '/home', function(){
-    return 'danu';
+    dump('home');
 });
 
 $router->add('GET|POST', '/projects', function(){
-    return 'danu';
+    dump('projects');
 });
 
-$router->add('PATCH', '/project/{id}', function(){
-    return 'danu';
+$router->add('PATCH', '/tag/{*}/{*}/{*}', function(){
+    dump('/danu/{*}/{*}');
 });
 
-$router->add('DELETE', '/tag/{id}', function(){
-    return 'danu';
+$router->add('DELETE', '/tag/3232/sfdsfds/fsdfsdf', function(){
+    dump('tag');
+});
+
+$router->add('DELETE', '/tag/{0-9}', function(){
+    dump('tag/0-9');
 });
 
 $router->run();
